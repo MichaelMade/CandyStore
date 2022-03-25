c671e8626b6a79947d7fefd3b125e44a587b49a1
Michael Moore <michael.moore@goya.com>
2022-03-25 14:32:22 -0400

    Initial Commit

---
+++ CandyStore/CandyStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandyStoreApp.swift
+//  CandyStore
+//
+//  Created by Michael Moore on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CandyStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
